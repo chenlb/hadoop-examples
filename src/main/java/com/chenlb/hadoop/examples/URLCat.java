65dd124734c17b9d8049467535746f8938248269
chenlb <chenlb2008@gmail.com>
2017-08-19 18:03:06 +0800

    url 和 file system

---
+++ src/main/java/com/chenlb/hadoop/examples/URLCat.java
@@ -0,0 +1,56 @@
+/*
+ * Copyright 2017 chenlb
+ *
+ *    Licensed under the Apache License, Version 2.0 (the "License");
+ *    you may not use this file except in compliance with the License.
+ *    You may obtain a copy of the License at
+ *
+ *        http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *    Unless required by applicable law or agreed to in writing, software
+ *    distributed under the License is distributed on an "AS IS" BASIS,
+ *    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *    See the License for the specific language governing permissions and
+ *    limitations under the License.
+ */
+
+package com.chenlb.hadoop.examples;
+
+import org.apache.hadoop.fs.FsUrlStreamHandlerFactory;
+import org.apache.hadoop.io.IOUtils;
+
+import java.io.InputStream;
+import java.net.MalformedURLException;
+import java.net.URL;
+
+/**
+ * @author chenlb
+ * @create 2017-08-16 20:04
+ */
+public class URLCat {
+
+	static {
+		URL.setURLStreamHandlerFactory(new FsUrlStreamHandlerFactory());
+	}
+
+	/**
+	 * <code>
+	 *     hadoop com.chenlb.hadoop.examples.URLCat file://`pwd`/example-resources/hadoop-version.txt
+	 * </code>
+	 * <br/>
+	 * <code>
+	 *     hadoop com.chenlb.hadoop.examples.URLCat hdfs://localhost/user/chenlb/example-resources/hadoop-version.txt
+	 * </code>
+	 *
+	 * @param args  url (file:///path/to/file or hdfs://xxx/path/to/file)
+	 */
+	public static void main(String[] args) throws Exception {
+		InputStream in = null;
+		try {
+			in = new URL(args[0]).openStream();
+			IOUtils.copyBytes(in, System.out, 4096, false);
+		} finally {
+			IOUtils.closeStream(in);
+		}
+	}
+}
